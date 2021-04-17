d4d40d1ee3b600de0c19e12cca62ee18b8fead58
이호진 <kiss0104040@gmail.com>
2021-04-18 00:48:32 +0900

    14.4 Welcome Screen

--- navigators/LoggedOutNav.tsx
+++ navigators/LoggedOutNav.tsx
@@ -15,7 +15,11 @@ const LoggedOutNav = () => {
         headerTintColor: 'black',
       }}
     >
-      <Stack.Screen name='Welcome' component={Welcome} />
+      <Stack.Screen
+        name='Welcome'
+        options={{ headerShown: false }}
+        component={Welcome}
+      />
       <Stack.Screen name='LogIn' component={LogIn} />
       <Stack.Screen name='CreateAccount' component={CreateAccount} />
     </Stack.Navigator>
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,7 +1,9 @@
 import { StackNavigationProp } from '@react-navigation/stack'
 import React, { VFC } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { LoggedOutNavParamList } from '../navigators/navigators'
+import styled from 'styled-components/native'
+import { colors } from '../colors'
 
 type WelcomeScreenNavigationProp = StackNavigationProp<
   LoggedOutNavParamList,
@@ -12,21 +14,50 @@ interface IProps {
   navigation: WelcomeScreenNavigationProp
 }
 
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+`
+
+const Logo = styled.Image`
+  max-width: 50%;
+  height: 100px;
+`
+
+const CreateAccount = styled.View`
+  background-color: ${colors.blue};
+  padding: 7px 10px;
+  border-radius: 5px;
+`
+const CreateAccountText = styled.Text`
+  color: white;
+  font-weight: 600;
+`
+
+const LoginLink = styled.Text`
+  color: ${colors.blue};
+  font-weight: 600;
+  margin-top: 10px;
+`
+
 const Welcome: VFC<IProps> = ({ navigation }) => {
+  const goToCreateAccount = () => navigation.navigate('CreateAccount')
+  const goToLogIn = () => navigation.navigate('LogIn')
+
   return (
-    <View>
-      <Text>Welcome</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
-        <View>
-          <Text>Go to Create Account</Text>
-        </View>
+    <Container>
+      <Logo resizeMode='contain' source={require('../assets/logo.png')} />
+      <TouchableOpacity onPress={goToCreateAccount}>
+        <CreateAccount>
+          <CreateAccountText>Create Account</CreateAccountText>
+        </CreateAccount>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('LogIn')}>
-        <View>
-          <Text>Go to Log In</Text>
-        </View>
+      <TouchableOpacity onPress={goToLogIn}>
+        <LoginLink>Log in</LoginLink>
       </TouchableOpacity>
-    </View>
+    </Container>
   )
 }
 
