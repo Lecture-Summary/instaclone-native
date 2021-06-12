478df612c2b50975af5f013b092aef36bbbb848f
이호진 <kiss0104040@gmail.com>
2021-06-13 00:22:08 +0900

    17.3 Select Photo part Two

--- src/screens/SelectPhoto.tsx
+++ src/screens/SelectPhoto.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import * as MediaLibrary from 'expo-media-library'
 import styled from 'styled-components/native'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Container = styled.View`
   flex: 1;
@@ -17,6 +20,34 @@ const Bottom = styled.View`
 `
 
 const SelectPhoto = () => {
+  const [ok, setOk] = useState(false)
+  const [photos, setPhotos] = useState<MediaLibrary.Asset[]>([])
+
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync()
+      setPhotos(photos)
+    }
+  }
+
+  const getPermissions = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync()
+    if (accessPrivileges === 'none' && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync()
+      if (accessPrivileges !== 'none') {
+        setOk(true)
+      }
+    } else if (accessPrivileges !== 'none') {
+      setOk(true)
+    }
+  }
+
+  useEffect(() => {
+    getPermissions()
+    getPhotos()
+  }, [])
+
   return (
     <Container>
       <Top></Top>
